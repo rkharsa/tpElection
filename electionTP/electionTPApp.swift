a1b793b536bbd63d832508bbdab4f9fba7b8a0e5
Rani kharsa <rkharsa@outlook.fr>
2021-11-13 21:06:56 +0100

    Initial Commit

---
+++ electionTP/electionTPApp.swift
@@ -0,0 +1,17 @@
+//
+//  electionTPApp.swift
+//  electionTP
+//
+//  Created by Rani Kharsa on 13/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct electionTPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
