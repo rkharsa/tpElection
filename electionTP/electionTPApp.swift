19a90e90fb4b709b6a4e2af20bfd9ecfb954593e
Rani kharsa <rkharsa@outlook.fr>
2021-12-13 00:04:39 +0100

    connection etablished

--- electionTP/electionTPApp.swift
+++ electionTP/electionTPApp.swift
@@ -6,9 +6,12 @@
 //
 
 import SwiftUI
-
+import Firebase
 @main
 struct electionTPApp: App {
+    init(){
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
